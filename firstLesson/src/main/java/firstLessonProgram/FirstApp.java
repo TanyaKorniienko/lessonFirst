19b9488b7d9059a85c5736cba72fbc65f491c762
Tanya <manualqa.tatiana@gmail.com>
2022-02-02 17:08:25 +0200

    First Commit

---
+++ firstLesson/src/main/java/firstLessonProgram/FirstApp.java
@@ -0,0 +1,20 @@
+package firstLessonProgram;
+
+public class FirstApp {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+
+        //int a = 10;
+        //int b = 20;
+        //int c = (a + b - 5) * 2;
+        //System.out.println("c = " + c);//
+
+        //int a;
+        //int b;
+        //a = 128;
+        //System.out.println("a = " + a);
+        //b = a / 2;
+        //System.out.println("b = a / 2 = " + b);
+
+    }
+}
